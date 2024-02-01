1cc3dc58444d0a61ee53b4e8eafaedb056998683
unknown <abhisheky220920@gmail.com>
2024-02-01 10:42:15 +0530

    Login regiatration code

--- controller/controller.js
+++ controller/controller.js
@@ -1,59 +1,4 @@
-// // controller.js
-// const bcrypt = require('bcrypt');
-// const jwt = require('jsonwebtoken');
-// const dataModel = require('../datamodel/datamodel');
-// const config = require('../config/config');
 
-// async function register(req, res) {
-//   const { username, password } = req.body;
-
-//   // Hash the password
-//   const hashedPassword = await bcrypt.hash(password, 10);
-
-//   // Store user information in the database
-//   try {
-//     const user = await dataModel.createUser(username, hashedPassword);
-//     res.json({ id: user.id, username: user.username });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// }
-
-// async function login(req, res) {
-//     const { username, password } = req.body;
-  
-//     // Retrieve user information from the database
-//     try {
-//       const user = await dataModel.getUserByUsername(username);
-  
-//       if (user) {
-//         // User found, check password
-//         const isPasswordValid = await bcrypt.compare(password, user.password);
-  
-//         if (isPasswordValid) {
-//           // Generate a JWT token
-//           const token = jwt.sign({ userId: user.id, username: user.username }, config.jwtSecret, { expiresIn: '1h' });
-//           res.json({ token });
-//         } else {
-//           // Password incorrect
-//           res.status(401).json({ error: 'Incorrect password' });
-//           console.log("invalid password");
-//         }
-//       } else {
-//         // User not found
-//         res.status(401).json({ error: 'User not found' });
-//         console.log("user not found");
-//       }
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).json({ error: 'Internal Server Error' });
-//     }
-//   }
-  
-
-// module.exports = { register, login };
-// controller.js
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const dataModel = require('../datamodel/datamodel');
@@ -63,10 +8,10 @@ async function register(req, res) {
   try {
     const { username, password } = req.body;
 
-    // Hash the password
+   
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Store user information in the database
+    
     const user = await dataModel.createUser(username, hashedPassword);
 
     res.json({ id: user.id, username: user.username });
--- server.js
+++ server.js
@@ -1,27 +1,7 @@
-// // server.js
-// const express = require('express');
-// const bodyParser = require('body-parser');
-
-// const routes = require('./route/route');
-// // config.js
-// require('dotenv').config();
-
-
-// const app = express();
-// const port = 3000;
-
-// app.use(bodyParser.json());
-
-// app.use('/', routes);
-
-// app.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// });
-// server.js
 const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config(); // Load environment variables from .env
-const config = require('./config/config');
+
 const routes = require('./route/route');
 
 const app = express();
